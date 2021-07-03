5755fe2571189c932c45abc593c455039f708558
claudiogaraycochea <computadoraweb@gmail.com>
2021-07-03 19:13:25 +0200

    Router pages

--- src/pages/private/about/About.tsx
+++ src/pages/private/about/About.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import IPage from '../../../models/page';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 
-function About() {
+const About: React.FunctionComponent<IPage & RouteComponentProps<any>> = props => {
+  const [message, setMessage] = useState < string > ('');
+
+  useEffect(() => {
+    let number = props.match.params.number;
+
+    if (number) {
+      setMessage(`The Number is ${number}`);
+    }
+    else {
+      setMessage(`No number provided!`);
+    }
+  }, [props])
+
   return (
     <div className="about">
-      About
+      About {message}
+      <Link to='/'>Go to home</Link>
     </div>
   );
 }
 
-export default About;
+export default withRouter(About);
